ee3041b92d648d46d338af59c98c7ab735d799f4
Artem Maevskiy <amaevskij@gmail.com>
2025-11-16 22:28:46 +0800

    optimized algorithm (analytical SL(3, Z) optimization)

--- supercell_finder/core.py
+++ supercell_finder/core.py
@@ -1,20 +1,8 @@
 from typing import Tuple
 
 import numpy as np
+print("I'M PATCHED!")
 
-
-_SL3Z_BASE_GENS = np.array([
-    [[0, 1, 0], [0, 0, 0], [0, 0, 0]],
-    [[0, 0, 1], [0, 0, 0], [0, 0, 0]],
-    [[0, 0, 0], [0, 0, 1], [0, 0, 0]],
-    [[0, 0, 0], [1, 0, 0], [0, 0, 0]],
-    [[0, 0, 0], [0, 0, 0], [1, 0, 0]],
-    [[0, 0, 0], [0, 0, 0], [0, 1, 0]],
-])
-SL3Z_BASE = np.concatenate([
-    np.eye(3, dtype=int) + _SL3Z_BASE_GENS,
-    np.eye(3, dtype=int) - _SL3Z_BASE_GENS,
-], axis=0)
 
 EPSILON = 1e-10
 MAX_OPT_ITERATIONS = 1000
@@ -48,28 +36,10 @@ def calculate_interplanar_distances(lattices: np.ndarray) -> np.ndarray:
     result = np.abs(vol[..., None] / np.linalg.norm(cross_vecs, axis=-1))
     return np.concatenate([result[..., 1:], result[..., :1]], axis=-1)
 
-def apply_SL3Z(transforms: np.ndarray) -> np.ndarray:
-    """
-    Apply every basic SL(3,Z) operation defined above to each transformation in `transforms`.
-
-    Parameters
-    ----------
-    transforms: np.ndarray
-        Array with 3d transformation matrices defined in the last two dimensions
-
-    Returns
-    -------
-    np.ndarray
-    """
-    ddim = transforms.ndim - 2
-    assert ddim >= 0
-    return SL3Z_BASE.reshape((-1,) + (1,) * ddim + (3, 3)) @ transforms[None]
 
 def optimize_trasforms(
     lattices: np.ndarray,
     transforms: np.ndarray,
-    depth: int,
-    verbose: bool = False,
 ) -> np.ndarray:
     """
     Given source lattices and their transformations, modify transformations (by SL(3, Z) operations only),
@@ -81,10 +51,6 @@ def optimize_trasforms(
         Source lattices (should be aligned with last dimensions of `transforms`, see assertions below)
     transforms: np.ndarray
         Transformations to optimize
-    depth: int
-        How many SL(3, Z) modifications to apply at a single step
-    verbose: bool
-        Controls whether to print the number of iterations taken during optimization
 
     Returns
     -------
@@ -96,48 +62,59 @@ def optimize_trasforms(
     assert transforms_ddims >= 0
     assert lattices.shape == transforms.shape[transforms_ddims:]
     assert lattices.shape[-2:] == (3, 3)
-    assert depth >= 1
 
     t_curr = transforms.copy()
-    num_iterations = 0
-    while True:
-        assert num_iterations < MAX_OPT_ITERATIONS
-        num_iterations += 1
-        t_exp = apply_SL3Z(t_curr)
-        for _ in range(1, depth):
-            shape = t_exp.shape
-            t_exp = apply_SL3Z(t_exp).reshape((len(SL3Z_BASE) * shape[0],) + shape[1:])
-        tt = np.concatenate([t_curr[None], t_exp], axis=0)
-        hh = calculate_interplanar_distances(tt @ lattices)  # 3 distances per structure
-        hh_min = hh.min(axis=-1)  # minimal distance per structure
 
-        # Exit condition: none of the modified transformations are better
-        if (hh_min[:1] + EPSILON >= hh_min[1:]).all():
-            break
-
-        # Assign positive score to transformations with largest minimal distance
-        # and select those with highest sum of distances
-        score = np.where(
-            np.isclose(hh_min, hh_min.max(axis=0, keepdims=True)),
-            hh.sum(axis=-1),
-            0,
+    for i in range(3):
+        super_lattices = t_curr @ lattices
+        orts = np.cross(
+            super_lattices,
+            np.concatenate([super_lattices[..., 1:, :], super_lattices[..., :1, :]], axis=-2)
         )
+        orts = np.concatenate([orts[..., 1:, :], orts[..., :1, :]], axis=-2)
 
-        ids_best = score.argmax(axis=0, keepdims=True)[..., None, None]
-        (t_curr,) = np.take_along_axis(tt, ids_best, axis=0)
-    if verbose:
-        print(f"Optimized in {num_iterations} steps.")
+        j, k = (i + 1) % 3, (i + 2) % 3
+        ha, hb, hc = (
+            orts[..., i, :],
+            orts[..., j, :],
+            orts[..., k, :],
+        )
+        bb = (hb * hb).sum(axis=-1)
+        cc = (hc * hc).sum(axis=-1)
+        ab = (ha * hb).sum(axis=-1)
+        ac = (ha * hc).sum(axis=-1)
+        bc = (hb * hc).sum(axis=-1)
 
-    assert t_curr.shape == transforms.shape
+        _denom = bb * cc - bc**2
+        beta = (ab * cc - ac * bc) / _denom
+        gamma = (ac * bb - ab * bc) / _denom
+
+        beta = np.stack([np.ceil(beta), np.floor(beta)], axis=0).astype(int)
+        gamma = np.stack([np.ceil(gamma), np.floor(gamma)], axis=0).astype(int)
+
+        obj = np.linalg.norm(ha - beta[:, None, ..., None] * hb - gamma[None, :, ..., None] * hc, axis=-1)
+        obj = obj.reshape(4, *obj.shape[2:])
+        idx_min = obj.argmin(axis=0, keepdims=True)
+        idx_beta, idx_gamma = np.unravel_index(idx_min, (2, 2))
+        beta = np.take_along_axis(beta, idx_beta, axis=0)
+        gamma = np.take_along_axis(gamma, idx_gamma, axis=0)
+
+        delta_transf = np.ones(shape=super_lattices.shape[:-2], dtype=int)[..., None, None] * np.eye(3, dtype=int)
+        delta_transf[..., j, i] = beta
+        delta_transf[..., k, i] = gamma
+
+        t_curr = delta_transf @ t_curr
+
     return t_curr
+
 
 def find_optimal_supercell(
     lattice: np.ndarray,
     d_inner: float,
-    depth: int = 1,
     minimal_volume_increase: int = 1,
     max_candidates: int = 1000,
     rng_seed: int = 42,
+    bandwidth: int = 3,
     verbose: bool = False,
 ) -> Tuple[np.ndarray, np.ndarray]:
     """
@@ -149,13 +126,15 @@ def find_optimal_supercell(
         Lattice defining the elementary cell
     d_inner: float
         Diameter of a sphere to fit in
-    depth: int
-        Depth passed to `optimize_trasforms` calls
+    minimal_volume_increase: int
+        Force lower bound on the determinant of the transformation (defaults to 1).
     max_candidates: int
         Limits the number of candidate transformations to optimize. Ignored if less than
         or equals to zero.
     rng_seed: int
         Random seed for selecting `max_candidates` transformations
+    bandwidth: int
+        Single search iteration will be in [D - bandwidth, D + bandwidth] determinant range.
     verbose: bool
         Controls whether to print out various info
 
@@ -169,6 +148,8 @@ def find_optimal_supercell(
     assert lattice.shape == (3, 3)
     assert d_inner > 0
     assert isinstance(minimal_volume_increase, int) and minimal_volume_increase >= 1
+    rng = np.random.default_rng(rng_seed)
+
     # The smallest possible cell fitting the sphere is a cube with side = d_inner.
     # Hence, this is the minimal volume change we need.
     det_lower_bound = np.ceil(
@@ -187,6 +168,8 @@ def find_optimal_supercell(
         diagonal_scalings += 1
         det_upper_bound = diagonal_scalings.prod()
 
+    if verbose:
+        print("Setting best to diagonal")
     best = np.diag(diagonal_scalings)
     if det_lower_bound != det_upper_bound:  # if so, there may be a better solution than diagonal
         assert det_lower_bound < det_upper_bound
@@ -196,10 +179,18 @@ def find_optimal_supercell(
         if verbose:
             print("Optimizing seed...")
 
-        seed_transform = optimize_trasforms(lattice, np.eye(3, dtype=int) * 5, depth=depth, verbose=verbose) / 5.0
+
+        seed_transform = optimize_trasforms(lattice, best) / det_i(best)**(1.0 / 3)
         candidates = np.round(
             target_dets[:, None, None]**(1.0 / 3) * seed_transform[None]
         ).astype(int)
+
+        candidates_structured = candidates.reshape(-1).view(
+            np.dtype([(str(i), candidates.dtype) for i in range(9)])
+        )
+        assert candidates_structured.shape == (len(candidates),)
+        _, unique_ids = np.unique(candidates_structured, return_index=True)
+        candidates = candidates[unique_ids]
 
         # Augment the transformation candidates with the neighbor lattice vector options
         # (this scales up the number of candidates by a factor of ~20k)
@@ -215,27 +206,50 @@ def find_optimal_supercell(
         valid_dets = (det_lower_bound <= dets) & (dets < det_upper_bound)
         if valid_dets.any():
             dets = dets[valid_dets]
-            candidates = candidates[valid_dets]
+            candidates_pool = candidates[valid_dets]
 
-            rng = np.random.default_rng(rng_seed)
-            if len(candidates) > max_candidates > 0:
-                ids = rng.choice(len(candidates), max_candidates, replace=False)
-                candidates = candidates[ids]
-                dets = dets[ids]
+            window_min = dets.min()
+            window_max = dets.max()
+            while True:
+                remaining_dets = dets[(dets >= window_min) & (dets <= window_max)]
+                if len(remaining_dets) == 0:
+                    if verbose:
+                        print("Can't improve further")
+                    break
+                attempt_det = np.quantile(
+                    np.unique(remaining_dets), 0.5, method="closest_observation"
+                )
 
-            if verbose:
-                print("Optimizing candidates...")
-            candidates = optimize_trasforms(lattice, candidates, depth=depth, verbose=verbose)
-            hh = calculate_interplanar_distances(candidates @ lattice).min(axis=-1)
+                selection = (
+                    dets >= max(attempt_det - bandwidth, window_min)
+                ) & (
+                    dets <= min(attempt_det + bandwidth, window_max)
+                )
+                current_candidates = candidates_pool[selection]
+                current_dets = dets[selection]
 
-            valid_hh = hh >= d_inner
-            if valid_hh.any():
-                candidates = candidates[valid_hh]
-                dets = dets[valid_hh]
-                best = candidates[dets.argmin()]
-            else:
+                assert len(current_candidates) > 0
+                if len(current_candidates) > max_candidates > 0:
+                    ids = rng.choice(len(current_candidates), max_candidates, replace=False)
+                    current_candidates = current_candidates[ids]
+                    current_dets = current_dets[ids]
+
                 if verbose:
-                    print("Fallback to diagonal (no valid hh)")
+                    print("Optimizing candidates...")
+                current_candidates = optimize_trasforms(lattice, current_candidates)
+                hh = calculate_interplanar_distances(current_candidates @ lattice).min(axis=-1)
+
+                valid_hh = hh >= d_inner
+                if valid_hh.any():
+                    if verbose:
+                        print("Found improvement")
+
+                    best = current_candidates[valid_hh][
+                        current_dets[valid_hh].argmin()
+                    ]
+                    window_max = det_i(best) - 1
+                else:
+                    window_min = current_dets.max() + 1
         else:
             if verbose:
                 print("Fallback to diagonal (no valid dets)")
